d57717245a73f34aa407488952d0c4b965922e28
Daniel Yepes <110424307+MakabeD@users.noreply.github.com>
2026-02-11 17:37:22 +0000

    remove corrupted data

---
+++ python/mobile-churn/src/data/preprocess.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+class preprocess():
+    def __innit__(self, datasource, dataset_info):
+        self.datasource = datasource
+        self.dataset_info = dataset_info
+
+    def clering(self, df, dataset_info):
+        return
+        
