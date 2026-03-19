a6a1e59c39622c28d3ad7c687215a09021fb56df
Daniel Yepes <110424307+MakabeD@users.noreply.github.com>
2026-03-19 20:06:13 +0000

    feat: mobile/telecom Churn API fix some files as config_path, and both api's main.py

--- python/mobile-churn/src/server/main.py
+++ python/mobile-churn/src/server/main.py
@@ -5,21 +5,22 @@ PATH_ROOT=Path(__file__).resolve().parent.parent.parent
 import sys 
 sys.path.append(str(PATH_ROOT))
 from src.inference.predictor_factory import PredictorFactory
-from src.inference.predictor import CreditScoringRiskPredictor
-from src.server.schemas import CreditScoringInput
+from src.inference.predictor import TelecomChurnPredictor
+from src.server.schemas import ChurnInput
 
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     app.state.predictor = PredictorFactory.build()
-
+    
     yield
 
     app.state.predictor = None
+
 app = FastAPI(lifespan=lifespan)
 
 @app.post('/predict')
-def inference(input:CreditScoringInput):
+def inference(input:ChurnInput):
     predictor_instance = app.state.predictor
     response = predictor_instance.predict(input_data=input)
     return response.model_dump(by_alias=True, mode="json")
